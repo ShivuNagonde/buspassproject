41a420c217bb8847c050bd0779424a9f024944eb
Shivakumar <Shivakumar@DESKTOP-9PDBQK6>
2021-03-15 11:11:35 +0530

    Mail otp login added

---
+++ mail-otp-login-example/src/main/java/com/spring/mailotploginexample/model/Login.java
@@ -0,0 +1,13 @@
+package com.spring.mailotploginexample.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Login {
+
+	
+	private int id;
+	private String email;
+	private String password;
+	
+}
